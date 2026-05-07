57c7fb38d361ac6c8ec0c9f8c4f395e8bcf066af
Sam Philipsborn <sp624@sussex.ac.uk>
2026-05-07 19:17:53 +0800

    adding correct filepaths

--- src/waves_angular_clustering/main.py
+++ src/waves_angular_clustering/main.py
@@ -1,13 +1,13 @@
 from angular_clustering import WavesWideClustering, AngularClusteringPlots
 
 ww = WavesWideClustering(
-    n_photom_filepath  = '/data/waves/WWN_photom.parquet',
-    s_photom_filepath  = '/data/waves/WWS_photom.parquet',
-    n_stargal_filepath = '/data/waves/WWN_stargal.csv',
-    s_stargal_filepath = '/data/waves/WWS_stargal.csv',
-    n_randoms_filepath = '/data/waves/WWN_randoms.parquet',
-    s_randoms_filepath = '/data/waves/WWS_randoms.parquet',
-    results_directory  = '/results/clustering',
+    n_photom_filepath  = '/mnt/lustre/projects/astro/general/sp624/waves-catas/d1m3p1f1/WAVES-N_d1m3p1f1.parquet',
+    s_photom_filepath  = '/mnt/lustre/projects/astro/general/sp624/waves-catas/d1m3p1f1/WAVES-S_d1m3p1f1.parquet',
+    n_stargal_filepath = '/research/astrodata/4most/WAVES/target_catalogues/star_gal_sep/WAVES-N_d1m3p1f1_Z22_stargal.parquet',
+    s_stargal_filepath = '/research/astrodata/4most/WAVES/target_catalogues/star_gal_sep/WAVES-S_d1m3p1f1_Z22_stargal.parquet',
+    n_randoms_filepath = '/mnt/lustre/projects/astro/general/sp624/waves_randoms/waves-wide_n_randoms.parquet',
+    s_randoms_filepath = '/mnt/lustre/projects/astro/general/sp624/waves_randoms/waves-wide_s_randoms.parquet',
+    results_directory  = '/mnt/lustre/projects/astro/general/sp624/angular_clustering_waves/results_20260507/',
 )
 
 # Runs all 6 combinations (3 target_selections × 2 ghostmask_selections).
