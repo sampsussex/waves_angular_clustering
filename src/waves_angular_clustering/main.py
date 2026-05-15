0da526bbf4d4f2c6ff76c659717cc6a5cf0ae857
Sam Philipsborn <sp624@sussex.ac.uk>
2026-05-15 10:02:46 +0800

    update main with new filepaths

--- src/waves_angular_clustering/main.py
+++ src/waves_angular_clustering/main.py
@@ -2,6 +2,8 @@ from angular_clustering import WavesWideClustering, AngularClusteringPlots
 
 
 if __name__ == '__main__':
+    print("Running WAVES angular clustering...")
+    print('Initializing clustering class with file paths and parameters...')
     ww = WavesWideClustering(
         n_photom_filepath  = '/mnt/lustre/projects/astro/general/sp624/waves-catas/d1m3p1f1/WAVES-N_d1m3p1f1.parquet',
         s_photom_filepath  = '/mnt/lustre/projects/astro/general/sp624/waves-catas/d1m3p1f1/WAVES-S_d1m3p1f1.parquet',
@@ -9,7 +11,8 @@ if __name__ == '__main__':
         s_stargal_filepath = '/research/astrodata/4most/WAVES/target_catalogues/star_gal_sep/WAVES-S_d1m3p1f1_Z22_stargal.parquet',
         n_randoms_filepath = '/mnt/lustre/projects/astro/general/sp624/waves_randoms/waves-wide_n_randoms.parquet',
         s_randoms_filepath = '/mnt/lustre/projects/astro/general/sp624/waves_randoms/waves-wide_s_randoms.parquet',
-        results_directory  = '/mnt/lustre/projects/astro/general/sp624/angular_clustering_waves/results_20260511b/',
+        results_directory  = '/mnt/lustre/projects/astro/general/sp624/angular_clustering_waves/results_20260515/',
     )
-
+    print('Getting clustering results for all selections to run...')
     all_results = ww.get_clustering_for_all_selections_to_run()
+    print('Completed')
